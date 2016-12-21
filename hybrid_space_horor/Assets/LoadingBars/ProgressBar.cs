4aea7ed488f50fb3d92c506e97f0058bb3602300
corderoit <tariquake@hotmail.com>
2016-12-21 20:36:29 +0100

    delay

--- hybrid_space_horor/Assets/LoadingBars/ProgressBar.cs
+++ hybrid_space_horor/Assets/LoadingBars/ProgressBar.cs
@@ -2,23 +2,34 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class ProgressBar : MonoBehaviour {
+public class ProgressBar : MonoBehaviour
+{
 
+    public int cameraCooldownTime;
     public int maxAmount;
 
     public Transform loadingBar;
     public Transform textLoading;
+
+    public Text textRecharging; // in plaats van text moet er een texture gebruikt worden, want anders verdwijnt het achter de fading;
+    [HideInInspector]
+    private Color textColor;
+    private bool inCooldown;
+
     [HideInInspector]
     private float currentAmount;
     [SerializeField]
-    private float speed;	
+    private float speed;
 
     void Start()
     {
+        textRecharging.color = new Color(1, 0, 0, 0);
+        inCooldown = false;
         currentAmount = maxAmount;
     }
 
-	void Update () {
+    void Update()
+    {
         if (Input.GetMouseButtonDown(0))
         {
             currentAmount = maxAmount;
@@ -28,11 +39,48 @@ public class ProgressBar : MonoBehaviour {
         {
             currentAmount -= speed * Time.deltaTime;
             textLoading.GetComponent<Text>().text = ((int)currentAmount).ToString() + "%";
-        } else
+        }
+        else
         {
             textLoading.GetComponent<Text>().text = ((int)currentAmount).ToString() + "%";
-            // put here the code to execute when the the bar is full
+            StartCoroutine(cameraCooldown());
+
         }
         loadingBar.GetComponent<Image>().fillAmount = currentAmount / 100;
-	}
+
+        if (inCooldown)
+        {
+            float blink = Mathf.Sin(Time.time * 3);
+            textRecharging.color = new Color(1, 0, 0, blink);
+        } else
+        {
+            textRecharging.color = new Color(1, 0, 0, 0);
+        }
+    }
+
+    void OnGUI()
+    {
+        if (inCooldown)
+        {
+            float blink = Mathf.Sin(Time.time * 3);
+            textRecharging.color = new Color(1, 0, 0, blink);
+        }
+        else
+        {
+            textRecharging.color = new Color(1, 0, 0, 0);
+        }
+    }
+
+    IEnumerator cameraCooldown()
+    {
+        float fadeTime = GameObject.Find("_GM").GetComponent<Fading>().BeginFade(1);
+        inCooldown = true;
+        yield return new WaitForSeconds(fadeTime);
+
+        yield return new WaitForSeconds(cameraCooldownTime);
+
+        inCooldown = false;
+        GameObject.Find("_GM").GetComponent<Fading>().BeginFade(-1);
+        currentAmount = maxAmount;
+    }
 }
